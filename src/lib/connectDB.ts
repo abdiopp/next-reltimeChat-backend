5f9263b0986374b280338090a104c6b3a15cd785
Abdullah Murtaza <ginnieabdullah007@gmail.com>
2024-01-14 12:15:46 +0500

    push2

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,12 +1,11 @@
 import prisma from "@/prisma";
-
-export const connectDB =async () => {
-    try {
-        await prisma.$connect()
-    } catch (error) {
-        console.log('====================================');
-        console.log(error);
-        console.log('====================================');
-        throw new Error('Error connecting')
-    }
-}
+export const connectDB = async () => {
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
+    throw new Error("Error connecting");
+  }
+};
